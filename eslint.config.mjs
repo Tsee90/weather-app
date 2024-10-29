c023d0269499c20c90c384bd1667ea8d5349a665
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-10-29 14:54:56 -0400

    Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  {
+    env: {
+      browser: true,
+      node: true,
+      es2021: true,
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+    },
+  },
 ];
